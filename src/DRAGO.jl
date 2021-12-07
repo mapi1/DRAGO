5fef9c66d64467dcecad08c612509d90ddf93bed
marius@dirk <marius.pille@hu-berlin.de>
2021-12-07 13:17:03 +0100

    Fix divergence to zero thanks to Jan

--- src/DRAGO.jl
+++ src/DRAGO.jl
@@ -3,7 +3,6 @@ module DRAGO
 using LinearAlgebra
 using Plots
 using Distributions
-using BenchmarkTools
 using BandedMatrices
 
 include("utility.jl")
@@ -41,6 +40,9 @@ function drago(y, λ1, λ2, S = I(length(y)); ΔFmin = 0.01, maxiter = 10, verbo
     xRes = S * y
     present = [1:N;][diag(S' * S) .== 1]
     Dn = Tridiagonal(ones(N-1), -2ones(N), ones(N-1))
+    Dn = Dn[2:end-1, 1:end] # make it N-2 x N
+    # Dn = BandedMatrix((0 => ones(N-2), 1 => -2ones(N-2), 2 => ones(N-2)), (N-2, N))
+    λ2DDn = λ2 .* Dn' * Dn
     
     # Objective function
     F(f, x) = 0.5norm2(S * y .- S * f .- x)^2 + 0.5λ1 * norm1(x) + .5λ2 *  norm2(Dn * f)^2
@@ -52,10 +54,10 @@ function drago(y, λ1, λ2, S = I(length(y)); ΔFmin = 0.01, maxiter = 10, verbo
     while ΔF > ΔFmin && iter <= maxiter
         A = Diagonal(λ1 ./ (abs.(xRes) .+ λ1))
         # B = Matrix(S' * A * S .+ λ2 .* Dn' * Dn) # faster for low N but more Memory gets allocated
-        B = BandedMatrix(S' * A * S .+ λ2 .* Dn' * Dn) # less Memory but apparently slower
+        B = BandedMatrix(S' * A * S .+ λ2DDn) # less Memory but apparently slower
         fRes = inv(B) * S' * A * S * y
         xRes = abs.(xRes) .* (S * (y .- fRes)) ./ (abs.(xRes) .+ λ1)
-        @show Fnow = F(fRes, xRes)
+        Fnow = F(fRes, xRes)
         verbose && @info "Iteration: $iter with F = $Fnow"
         ΔF = Fold .- Fnow
         Fold = Fnow 
--- src/example.jl
+++ src/example.jl
@@ -1,21 +1,49 @@
 # Recreate figure 3.3 from the original paper
+using Main.DRAGO
+using Plots
+using LinearAlgebra
+using Distributions
+using BandedMatrices
+using BenchmarkTools
 N = 1000
-t = range(0, 50, length = N)
+t = range(0, 55, length = N)
 f = -sin.(t) .- cos.(0.5t)
 (y, f, x, w, S) = simulateData(f, percGaps = 0.45, maxGapRatio = 0.02, outlierStrength = 1.5)
 present = [1:N;][diag(S' * S) .== 1]
 
 (fRes, xRes, residual) = drago(y, 0.6, 500, S, ΔFmin = 10e-2)
 
+# L1 norm
+norm1(x) = sum(abs, x)
+
+# L2 norm
+norm2(x) = sqrt(sum(abs2, x))
+
+
 # Plotting
 begin
     p1 = plot(f, lab = "Simulated")
     p2 = scatter(present, S * y, lab = "Noisy")
     p3 = scatter(present, S * y, lab = "", color = :lightgrey, alpha = 0.2)
+    rmse = norm2(f .- fRes) / norm2(f) 
     plot!(fRes, color = :black, linewidth = 2, lab = "Smoothed signal with RMSE: $(round(rmse, digits = 2))")
     p4 = plot(x[present], color = :red, lab = "True outlier")
     plot!(xRes, color = :black, lab = "Estimated outlier")
-    rmse = norm2(f .- fRes) / norm2(f) 
     p5 = plot(residual, lab = "Residual", color = :black)
     plot(p1, p2, p3, p4, p5, ylims = (-3, 3), layout = (5, 1), size = (800, 800))
 end
+
+N = 5
+Dn = Tridiagonal(ones(N-2), -2ones(N-1), ones(N-2))
+Dn' * Dn
+d = copy(Dn[2:end-1, 1:end])
+
+Dn[1,1] = -1
+Dn[end,end] = -1
+Dn' * Dn
+
+d' * d
+
+BandedMatrix((0 => ones(N-2), 1 => -2ones(N-2), 2 => ones(N-2)), (N-2, N))
+@benchmark (fRes, xRes, residual) = drago(y, 0.6, 500, S, ΔFmin = 10e-2)
+@profview (fRes, xRes, residual) = drago(y, 0.6, 500, S, ΔFmin = 10e-2)
